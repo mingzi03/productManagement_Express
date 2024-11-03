6dc3c6d0e836a5766ccc9c9b35d50d8b53bf9c96
newbiehmt <huuminh02231103@gmail.com>
2024-11-03 22:48:21 +0700

    logout

--- controllers/admin/auth.controller.js
+++ controllers/admin/auth.controller.js
@@ -46,3 +46,11 @@ module.exports.loginPost = async (req,res) => {
     res.cookie("token", user.token)
     res.redirect(`${systemConfig.prefixAdmin}/dashboard`);
 }
+
+// [GET] /admin/auth/logout
+module.exports.logout = (req,res) => {
+    //Xoa token trong cookie
+    res.clearCookie("token");
+
+    res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
+}
