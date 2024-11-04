e4eb57c50f32736bf6b4ddd997536dcc75629bf6
newbiehmt <huuminh02231103@gmail.com>
2024-11-04 23:27:03 +0700

    phan quyen

--- middlewares/admin/auth.middleware.js
+++ middlewares/admin/auth.middleware.js
@@ -1,5 +1,7 @@
-const systemConfig = require("../../config/system");
 const Account = require("../../models/account.model");
+const Role = require("../../models/role.model");
+
+const systemConfig = require("../../config/system");
 
 module.exports.requireAuth = async (req, res, next) => {
     //console.log(req.cookies.token);
@@ -8,10 +10,18 @@ module.exports.requireAuth = async (req, res, next) => {
         res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
     } else {
         //console.log(req.cookies.token);
-        const user = await Account.findOne({ token: req.cookies.token });
+        const user = await Account.findOne({ token: req.cookies.token }).select("-password");
         if (!user) {
             res.redirect(`${systemConfig.prefixAdmin}/auth/login`);
         } else {
+            const role = await Role.findOne({
+               _id: user.role_id
+            }).select("title permissions");
+
+            //Tao bien toan cuc
+            res.locals.user = user;
+            res.locals.role = role;
+
             next();
         }
     }
