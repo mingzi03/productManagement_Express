31f402f1575a3a471b8000f3cdcf2cbff1afc4e9
newbiehmt <huuminh02231103@gmail.com>
2024-11-05 21:02:21 +0700

    Danh sach san pham moi

--- controllers/client/home.contoller.js
+++ controllers/client/home.contoller.js
@@ -15,11 +15,21 @@ module.exports.index = async (req,res) => {
         status: "active"
     }).limit(3);  //.limit(number): gioi han so san pham render ra giao dien
     //console.log(productsFeatured);
-    const newProducts = productsHelper.priceNewProducts(productsFeatured);
+    const newProductsFeatured = productsHelper.priceNewProducts(productsFeatured);
     //End lay ra san pham noi bat
+
+    //Hien thi danh sach san pham moi nhat
+    const produtsNew = await Product.find({
+        deleted: false,
+        status: "active"
+    }).sort({ position: "desc" }).limit(6);
+
+    const newProductsNew = productsHelper.priceNewProducts(produtsNew);
+    //End hien thi danh sach san pham moi nhat
 
     res.render("client/pages/home/index", {
         pageTitle: "Trang chủ",
-        productsFeatured: newProducts
+        productsFeatured: newProductsFeatured,
+        produtsNew: newProductsNew
     });
 }
