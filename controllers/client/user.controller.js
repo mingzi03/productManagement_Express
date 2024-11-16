089febc36696f8a0ff94c6fbc22730311c8ca58c
newbiehmt <huuminh02231103@gmail.com>
2024-11-17 02:13:38 +0700

    Login Client

--- controllers/client/user.controller.js
+++ controllers/client/user.controller.js
@@ -1,6 +1,5 @@
 const md5 = require("md5");
 const User = require("../../models/user.model");
-const { use } = require("../../routes/client/user.route");
 
 // [GET] /user/register
 module.exports.register = async (req,res) => {
@@ -33,4 +32,47 @@ module.exports.registerPost = async (req,res) => {
     res.cookie("tokenUser", user.tokenUser);
 
     res.redirect("/");
+}
+
+// [GET] /user/login
+module.exports.login = async (req,res) => {
+    res.render("client/pages/user/login", {
+        pageTitle: "Đăng nhập tài khoản",
+    });
+}
+
+// [POST] /user/login
+module.exports.loginPost = async (req,res) => {
+    //console.log(req.body);
+
+    const email = req.body.email;
+    const password = req.body.password;
+
+    const user = await User.findOne({
+        email: email,
+        deleted: false
+    });
+
+    if (!user) {
+        req.flash("error", "Email không tồn tại!");
+        res.redirect("back");
+        return;
+    }
+
+    if (md5(password) !== user.password) {
+        req.flash("error", "Mật khẩu không tồn tại!");
+        res.redirect("back");
+        return;
+    }
+
+    if (user.status === "locked") {
+        req.flash("error", "Tài khoản đã bị khóa!");
+        res.redirect("back");
+        return;
+    }
+
+    res.cookie("tokenUser", user.tokenUser);
+    
+    res.redirect("/");
+
 }
